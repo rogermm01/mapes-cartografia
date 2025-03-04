8de90ae6c7768304dcac8d7c8d1292d22eb31558
rogermm01 <roger.montane80@gmail.com>
2025-03-04 12:36:59 +0100

    Update poblacio_creixement_Catalunya_2023.sql

--- SQL/poblacio_creixement_Catalunya_2023.sql
+++ SQL/poblacio_creixement_Catalunya_2023.sql
@@ -1,19 +1,4 @@
-**Les taules han estat processades abans de la seva importació a la base de dades (PostgreSQL)
-
---CREACIÓ DE LES COLUMNES AMB LES DADES DE NAIXEMENTS, DEFUNCIONS, POBLACIÓ I CREIXEMEN VEGETATIU
-
-ALTER TABLE catalunya.limits_comarcals ADD COLUMN naixements_2023 NUMERIC;
-
-ALTER TABLE catalunya.limits_comarcals ADD COLUMN defuncions_2023 NUMERIC;
-
-ALTER TABLE catalunya.limits_comarcals ADD COLUMN poblacio_2023 NUMERIC;
-
-ALTER TABLE catalunya.limits_comarcals ADD COLUMN creixement_vegetatiu_1000h_2023 NUMERIC;
-
-ALTER TABLE catalunya.limits_comarcals ADD COLUMN poblacio_2023_percentatge DOUBLE PRECISION;
-
-
---INSERCIÓ DE LES DADES EN LES NOVES COLUMNES, A PARTIR DEL NOM DE COMARCA
+--INSERCIÓ DE LES DADES EN LES NOVES COLUMNES, A PARTIR DEL NOM DE COMARCA / INSERCIÓN DE LOS DATOS EN LAS NUEVAS COLUMNAS, A PARTIR DEL NOMBRE DE COMARCA 
 
 UPDATE catalunya.limits_comarcals as c
 SET naixements_2023 = n.total
@@ -30,11 +15,16 @@ SET poblacio_2023 = p.total
 FROM catalunya.poblacio_2023 as p
 WHERE nom_comarca = nomcomar;
 
---CÀLCUL DEL CREIXEMENT VEGETATIU PER CADA 1000 HABITANTS (A PARTIR DE LA FÓRMULA EMPRADA PER L'INE, Instituto Nacional de Estadística)
+--CÀLCUL DEL CREIXEMENT VEGETATIU PER CADA 1000 HABITANTS / CÁLCULO DEL CRECIMIENTO VEGETATIVO POR CADA 1000 HABITANTES 
 
 UPDATE catalunya.limits_comarcals AS c
 SET creixement_vegetatiu_1000h_2023 = ((naixements_2023 - defuncions_2023) / (poblacio_2023) * 1000)
 WHERE poblacio_2023 IS NOT NULL;
+
+--CÀLCUL DEL PERCENTATGE DE POBLACIÓ PER COMARCA RESPECTE EL TOTAL / CÁLCULO DEL PORCENTAJE DE POBLACIÓN POR COMARCA RESPECTO EL TOTAL
+
+UPDATE catalunya.limits_comarcals AS c
+SET poblacio_2023_percentatge = (poblacio_2023 / (SELECT SUM (poblacio_2023) FROM catalunya.limits_comarcals) * 100);
 
 --CÀLCUL DEL PERCENTATGE DE POBLACIÓ PER COMARCA RESPECTE EL TOTAL
 
