489e35806df9c6710f367078f927d6a18906cee8
rogermm01 <roger.montane80@gmail.com>
2025-03-06 11:41:47 +0100

    Update poblacio_creixement_Catalunya_2023.sql

--- SQL/poblacio_creixement_Catalunya_2023.sql
+++ SQL/poblacio_creixement_Catalunya_2023.sql
@@ -1,3 +1,15 @@
+**Les taules han estat processades abans de la seva importació a la base de dades (PostgreSQL) / Las tablas han sido procesadas antes de su importación a la base de datos (PostgreSQL).
+
+--CREACIÓ DE LES COLUMNES AMB LES DADES DE NAIXEMENTS, DEFUNCIONS, POBLACIÓ I CREIXEMEN VEGETATIU / CREACIÓN DE LAS COLUMNAS CON LOS DATOS DE NACIMIENTOS, DEFUNCIONES, POBLACIÓN I CRECIMIENTO VEGETATIVO
+
+ALTER TABLE catalunya.limits_comarcals ADD COLUMN naixements_2023 NUMERIC;
+
+ALTER TABLE catalunya.limits_comarcals ADD COLUMN defuncions_2023 NUMERIC;
+
+ALTER TABLE catalunya.limits_comarcals ADD COLUMN poblacio_2023 NUMERIC;
+
+ALTER TABLE catalunya.limits_comarcals ADD COLUMN creixement_vegetatiu_1000h_2023 NUMERIC;
+
 --INSERCIÓ DE LES DADES EN LES NOVES COLUMNES, A PARTIR DEL NOM DE COMARCA / INSERCIÓN DE LOS DATOS EN LAS NUEVAS COLUMNAS, A PARTIR DEL NOMBRE DE COMARCA 
 
 UPDATE catalunya.limits_comarcals as c
@@ -20,13 +32,3 @@ WHERE nom_comarca = nomcomar;
 UPDATE catalunya.limits_comarcals AS c
 SET creixement_vegetatiu_1000h_2023 = ((naixements_2023 - defuncions_2023) / (poblacio_2023) * 1000)
 WHERE poblacio_2023 IS NOT NULL;
-
---CÀLCUL DEL PERCENTATGE DE POBLACIÓ PER COMARCA RESPECTE EL TOTAL / CÁLCULO DEL PORCENTAJE DE POBLACIÓN POR COMARCA RESPECTO EL TOTAL
-
-UPDATE catalunya.limits_comarcals AS c
-SET poblacio_2023_percentatge = (poblacio_2023 / (SELECT SUM (poblacio_2023) FROM catalunya.limits_comarcals) * 100);
-
---CÀLCUL DEL PERCENTATGE DE POBLACIÓ PER COMARCA RESPECTE EL TOTAL
-
-UPDATE catalunya.limits_comarcals AS c
-SET poblacio_2023_percentatge = (poblacio_2023 / (SELECT SUM (poblacio_2023) FROM catalunya.limits_comarcals) * 100);
